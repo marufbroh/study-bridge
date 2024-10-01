87ddd298b1a77eeabf1eded39db2db4cd352fe94
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-01 22:05:16 +0600

    Model Creation and Query Testing

--- queries/courses.js
+++ queries/courses.js
@@ -1,6 +1,26 @@
+import { Category } from "@/model/category-model";
 import { Course } from "@/model/course-model";
+import { Module } from "@/model/module.model";
+import { Testimonial } from "@/model/testimonial-model";
+import { User } from "@/model/user-model";
 
 export const getCourses = async () => {
-  const courses = await Course.find({});
+  const courses = await Course.find({})
+    .populate({
+      path: "category",
+      model: Category,
+    })
+    .populate({
+      path: "instructor",
+      model: User,
+    })
+    .populate({
+      path: "modules",
+      model: Module,
+    })
+    .populate({
+      path: "testimonials",
+      model: Testimonial,
+    });
   return courses;
 };
