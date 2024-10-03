eac1bf1fce4a06c28785339933fd3f33a2c83858
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-03 09:59:04 +0600

    home page course category section complete

--- queries/courses.js
+++ queries/courses.js
@@ -1,11 +1,21 @@
+import { replaceMongoIdInArray } from "@/lib/convertData";
 import { Category } from "@/model/category-model";
 import { Course } from "@/model/course-model";
 import { Module } from "@/model/module.model";
 import { Testimonial } from "@/model/testimonial-model";
 import { User } from "@/model/user-model";
 
-export const getCourses = async () => {
+export const getCourseList = async () => {
   const courses = await Course.find({})
+    .select([
+      "title",
+      "subtitle",
+      "thumbnail",
+      "modules",
+      "price",
+      "category",
+      "instructor",
+    ])
     .populate({
       path: "category",
       model: Category,
@@ -21,6 +31,6 @@ export const getCourses = async () => {
     .populate({
       path: "testimonials",
       model: Testimonial,
-    });
-  return courses;
+    }).lean();
+  return replaceMongoIdInArray(courses);
 };
