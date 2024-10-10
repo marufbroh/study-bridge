d22c0a2c0aa787c78d4ecee2949bc4c61f6d779e
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-10 21:12:31 +0600

    stripe integration

---
+++ components/enroll-course.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { ArrowRight } from "lucide-react";
+import { Button, buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
+import { createCheckoutSession } from "@/app/actions/stripe";
+
+export const EnrollCourse = ({ asLink }) => {
+  const formAction = async (data) => {
+    const { url } = await createCheckoutSession(data);
+
+    window.location.assign(url);
+  };
+
+  return (
+    <>
+      <form action={formAction}>
+        {asLink ? (
+          <Button
+            type="submit"
+            variant="ghost"
+            className="text-xs text-sky-700 h-7 gap-1"
+          >
+            Enroll
+            <ArrowRight className="w-3" />
+          </Button>
+        ) : (
+          <Button
+            type="submit"
+            href=""
+            className={cn(buttonVariants({ size: "lg" }))}
+          >
+            Enroll Now
+          </Button>
+        )}
+      </form>
+    </>
+  );
+};
