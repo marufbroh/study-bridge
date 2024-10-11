4492119e0be38dc4c326f6ad3392682e6475db83
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-11 19:56:36 +0600

    profile personal details form value state management

--- app/(main)/account/@tabs/page.js
+++ app/(main)/account/@tabs/page.js
@@ -1,11 +1,16 @@
+import { auth } from "@/auth";
 import ChangePassword from "../component/change-password";
 import ContactInfo from "../component/contact-info";
 import PersonalDetails from "../component/personal-details";
+import { getUserByEmail } from "@/queries/users";
 
-function Profile() {
+async function Profile() {
+	const session = await auth();
+	const loggedInUser = await getUserByEmail(session?.user?.email);
+
   return (
     <>
-      <PersonalDetails />
+      <PersonalDetails userInfo={loggedInUser}/>
       <div className="p-6 rounded-md shadow dark:shadow-gray-800 bg-white dark:bg-slate-900 mt-[30px]">
         <div className="grid lg:grid-cols-2 grid-cols-1 gap-5">
           <ContactInfo />
