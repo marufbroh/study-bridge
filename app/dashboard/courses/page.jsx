6afa3a5779a7c977e6ad3345e6c60e2fd15080ab
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-15 21:59:39 +0600

    course review data done

--- app/dashboard/courses/page.jsx
+++ app/dashboard/courses/page.jsx
@@ -1,13 +1,12 @@
-import { getInstructorDashboardData } from "@/lib/dashboard-helper";
+import { COURSE_DATA, getInstructorDashboardData } from "@/lib/dashboard-helper";
 import { columns } from "./_components/columns";
 import { DataTable } from "./_components/data-table";
 
 const CoursesPage = async () => {
-  const data = await getInstructorDashboardData();
-  // console.log(data);
+  const courses = await getInstructorDashboardData(COURSE_DATA);
   return (
     <div className="p-6">
-      <DataTable columns={columns} data={data?.courses} />
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
