bea7aaf654de5cec852f041a98ab640e8e7ab69d
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-19 21:02:42 +0600

    module model refactored

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import fs from "fs";
 import { pipeline } from "stream";
 import { promisify } from "util";
+
 import { updateCourse } from "@/app/actions/course";
 
 const pump = promisify(pipeline);
@@ -9,12 +10,11 @@ const pump = promisify(pipeline);
 export async function POST(request, response) {
   try {
     const formData = await request.formData();
-    const file = formData.get("file");
-
+    const file = formData.get("files");
     const destination = formData.get("destination");
 
     if (!destination) {
-      return new NextResponse("Destination is not specified", {
+      return new NextResponse("Destination not provided", {
         status: 500,
       });
     }
@@ -23,10 +23,14 @@ export async function POST(request, response) {
 
     await pump(file.stream(), fs.createWriteStream(filePath));
 
-    const courseId = formData.get("courseId")
-    await updateCourse(courseId, {thumbnail: file.name})
+    // This can be decoupled to another
+    // route handler
+    const courseId = formData.get("courseId");
+    await updateCourse(courseId, { thumbnail: file.name });
 
-    return new NextResponse(`File ${file.name} uploaded successfully`);
+    return new NextResponse(`File ${file.name} uploaded successfully`, {
+      status: 200,
+    });
   } catch (error) {
     return new NextResponse(error.message, {
       status: 500,
