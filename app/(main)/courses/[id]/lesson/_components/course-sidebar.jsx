3618cd71af295462eee57f5c42789377f58707ec
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-28 12:31:16 +0600

    Lesson side bar actual data, Lesson states maintained, ⁠Lesson unique URL

--- app/(main)/courses/[id]/lesson/_components/course-sidebar.jsx
+++ app/(main)/courses/[id]/lesson/_components/course-sidebar.jsx
@@ -2,8 +2,43 @@ import { CourseProgress } from "@/components/course-progress";
 import DownloadCertificate from "./download-certificate";
 import GiveReview from "./give-review";
 import SidebarModules from "./sidebar-modules";
+import { getCourseDetails } from "@/queries/courses";
+import { Watch } from "@/model/watch-model";
+import { getLoggedInUser } from "@/lib/loggedin-user";
 
-export const CourseSidebar = () => {
+export const CourseSidebar = async ({ courseId }) => {
+  const course = await getCourseDetails(courseId);
+  const loggedInUser = await getLoggedInUser();
+
+  const updatedModules = await Promise.all(
+    course.modules.map(async (module) => {
+      const moduleId = module?._id.toString();
+      const lessons = module?.lessonIds;
+
+      const updatedLessons = await Promise.all(
+        lessons.map(async (lesson) => {
+          const lessonId = lesson?._id.toString();
+
+          const watch = await Watch.findOne({
+            lesson: lessonId,
+            module: moduleId,
+            user: loggedInUser.id,
+          });
+
+          if (watch?.state) {
+            lesson.state = "completed";
+          }
+
+          return lesson;
+        })
+      );
+
+      return module;
+    })
+  );
+
+
+
   return (
     <>
       <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
@@ -17,11 +52,11 @@ export const CourseSidebar = () => {
           }
         </div>
 
-        <SidebarModules />
+        <SidebarModules courseId={courseId} modules={updatedModules} />
 
         <div className="w-full px-6">
-          <DownloadCertificate />
-          <GiveReview />
+          <DownloadCertificate courseId={courseId} />
+          <GiveReview  courseId={courseId}/>
         </div>
       </div>
     </>
--- app/(main)/courses/[id]/lesson/_components/sidebar-lessons.jsx
+++ app/(main)/courses/[id]/lesson/_components/sidebar-lessons.jsx
@@ -1,12 +1,23 @@
 import { AccordionContent } from "@/components/ui/accordion";
 import SidebarLessonItems from "./sidebar-lesson-items";
+import { replaceMongoIdInArray } from "@/lib/convertData";
 
-const SidebarLessons = () => {
- 
+const SidebarLessons = ({ courseId, lessons, moduleSlug }) => {
+  const allLessons = replaceMongoIdInArray(lessons).toSorted(
+    (a, b) => a.order - b.order
+  );
+
   return (
     <AccordionContent>
       <div className="flex flex-col w-full gap-3">
-        <SidebarLessonItems/>
+        {allLessons.map((lesson) => (
+          <SidebarLessonItems
+            key={lesson?.id}
+            courseId={courseId}
+            moduleSlug={moduleSlug}
+            lesson={lesson}
+          />
+        ))}
       </div>
     </AccordionContent>
   );
